1942f1c36d833971be2c97db342edbdc72920bdb
minjung Kim <ikwmjj@naver.com>
2021-10-29 16:08:34 +0900

    Add files via upload

---
+++ block.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+	int a = 3;
+	cout << "첫번째 출력" << a << endl;
+	{
+		int a = 4;
+		cout << "두번째 출력" << a << endl;
+
+	}
+	cout << "세번째 출력" << a << endl;
+}
